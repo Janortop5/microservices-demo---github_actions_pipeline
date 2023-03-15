e117937f42c4a9f9b6b0729f7e4396adb32aa731
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-03-15 21:03:53 +0000

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -42,7 +42,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 
 unzip awscliv2.zip
 
-sudo ./aws/install
+./aws/install
+
+mkdir ~/.aws/
 
 cp $AWS_CONFIG ~/.aws/config
 
