e7859131e787d41c18e3cb1ac4d53564ad6f116d
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-03-16 19:40:18 +0000

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -28,6 +28,16 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 kubectl version --client
 
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+
+apt-get install apt-transport-https --yes
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+apt-get update
+
+apt-get install helm
+
 apt install unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
