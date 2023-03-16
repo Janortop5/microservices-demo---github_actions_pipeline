1b8a89f42ce2ac23ad9b83f31f9a7be63d106827
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-03-16 15:39:12 +0000

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -28,16 +28,6 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 kubectl version --client
 
-curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_linux_amd64
-
-chmod +x ./aws-iam-authenticator
-
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
-
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-
-aws-iam-authenticator help
-
 apt install unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
