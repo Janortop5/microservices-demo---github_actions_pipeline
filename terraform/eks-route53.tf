424a394aeea4742cb2b26e46475c76be3da9c954
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-03-17 21:34:05 +0000

    Update eks-route53.tf

--- terraform/eks-route53.tf
+++ terraform/eks-route53.tf
@@ -11,17 +11,9 @@ data "kubernetes_service" "ingress-service" {
   ]
 }
 
-resource "aws_route53_record" "eks-record-1" {
+resource "aws_route53_record" "eks-record" {
   zone_id = aws_route53_zone.eks-hosted_zone.zone_id
-  name    = var.domain.record-1
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.kubernetes_service.ingress-service.status.0.load_balancer.0.ingress.0.hostname]
-}
-
-resource "aws_route53_record" "eks-record-2" {
-  zone_id = aws_route53_zone.eks-hosted_zone.zone_id
-  name    = var.domain.record-2
+  name    = var.domain.record
   type    = "CNAME"
   ttl     = "300"
   records = [data.kubernetes_service.ingress-service.status.0.load_balancer.0.ingress.0.hostname]
