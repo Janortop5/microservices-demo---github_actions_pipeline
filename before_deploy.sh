fa939e362d907fedefdd5099ccf3264f23625499
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-03-18 06:56:52 +0000

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -56,4 +56,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 
 helm repo add stable https://charts.helm.sh/stable
 
+helm repo add grafana https://grafana.github.io/helm-charts
+
 helm repo update
